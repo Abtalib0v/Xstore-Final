1b0005379719caeff53e4cc18bf66ec9b7623014
Adil Abtalıbov <100897751+Abtalib0v@users.noreply.github.com>
2025-08-21 22:53:59 +0400

    fix:Dialog

--- client/src/app/_common/Dialog/index.tsx
+++ client/src/app/_common/Dialog/index.tsx
@@ -1,27 +1,27 @@
-// import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-// import { FC } from "react";
-// import { DialogTypeProps } from "./type";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { FC } from "react";
+import { DialogTypeProps } from "./type";
 
-// export const CommonDialog: FC<DialogTypeProps> = ({title,desc,children,open,onClose}) =>{
-//     return (
-//         <Dialog open={open} onOpenChange={onClose}>
-//             <div>
-//                 <DialogTrigger asChild>
-//                 </DialogTrigger>
-//                 <DialogContent className="sm:max-w-[425px]">
-//                     <DialogHeader>
-//                         <DialogTitle>
-//                             {title}
-//                         </DialogTitle>
-//                         <DialogDescription>
-//                             {desc}
-//                         </DialogDescription>
-//                     </DialogHeader>
-//                     <div>
-//                         {children}
-//                     </div>
-//                 </DialogContent>
-//             </div>
-//         </Dialog>
-//     )
-// }
+export const CommonDialog: FC<DialogTypeProps> = ({title,desc,children,open,onClose}) =>{
+    return (
+        <Dialog open={open} onOpenChange={onClose}>
+            <div>
+                <DialogTrigger asChild>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>
+                            {title}
+                        </DialogTitle>
+                        <DialogDescription>
+                            {desc}
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div>
+                        {children}
+                    </div>
+                </DialogContent>
+            </div>
+        </Dialog>
+    )
+}
